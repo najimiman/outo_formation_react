6ea4207b66790a87d6c8e6108c2c84741b55e1ba
najimiman <114479872+najimiman@users.noreply.github.com>
2022-11-15 16:56:45 +0100

    outoformation react

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <h1>hello word</h1>
-  );
+class App extends Component {
+  render(){
+    return (
+      <h1>hello word</h1>
+    );
+  }
+ 
 }
 
 export default App;

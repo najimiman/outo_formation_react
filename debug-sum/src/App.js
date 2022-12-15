7f8c65c1320cd76732a138197b4a66993ff078b0
najimiman <114479872+najimiman@users.noreply.github.com>
2022-12-15 15:57:51 +0100

    debug sum

---
+++ debug-sum/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const a=5
+  const b=2
+  const z=a+b
+  return (
+    <div>
+
+      <h1>la resulta de a + b={z}</h1>
+    </div>
+  );
+}
+
+export default App;
